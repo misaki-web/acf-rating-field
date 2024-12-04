646ffadb12b9adecd1e44455502e0fc245276d5f
Misaki <64830812+misaki-web@users.noreply.github.com>
2024-12-04 12:50:30 -0500

    v1.0.9

--- acf-rating-field.php
+++ acf-rating-field.php
@@ -5,7 +5,7 @@
  * Description: Rating field for ACF supporting decimal numbers and custom icon (default icon is a star: ★).
  * Text Domain: acf-rating-field
  * Author: Misaki F.
- * Version: 1.0.8
+ * Version: 1.0.9
  */
 
 namespace AcfRatingField;
@@ -18,7 +18,7 @@ if (!defined('ABSPATH')) {
 # @title Constants
 ################################################################################
 
-define('ACF_RATING_FIELD_VERSION', '1.0.8');
+define('ACF_RATING_FIELD_VERSION', '1.0.9');
 
 ################################################################################
 ## @title Inclusions
